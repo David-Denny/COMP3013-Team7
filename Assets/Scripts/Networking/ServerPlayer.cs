3fec394afe6d7c976d1bf3f490f80b50a02ee687
Thomas Beacock <thomasbeacock@gmail.com>
2022-12-12 13:37:31 +0000

    Added multiple player spawn locations

--- Assets/Scripts/Networking/ServerPlayer.cs
+++ Assets/Scripts/Networking/ServerPlayer.cs
@@ -5,6 +5,9 @@ using Unity.Netcode;
 
 public class ServerPlayer : NetworkBehaviour
 {
+    private static int _spawnIndex = 0;
+    private static Vector3[] _spawnPoints = new Vector3[] { new(2.5f, 0.0f, 0.0f), new(2.5f, 7.0f, 0.0f) };
+
     public override void OnNetworkSpawn()
     {
         base.OnNetworkSpawn();
@@ -16,8 +19,15 @@ public class ServerPlayer : NetworkBehaviour
         }
 
         var client = GetComponent<ClientPlayer>();
-        var spawnPosition = new Vector3(2, 2, 0);
+        var spawnPosition = NextSpawn();
         client.SetSpawnClientRpc(spawnPosition,
             new ClientRpcParams() { Send = new ClientRpcSendParams() { TargetClientIds = new[] { OwnerClientId } } });
+    }
+
+    private Vector3 NextSpawn()
+    {
+        Vector3 point = _spawnPoints[_spawnIndex];
+        _spawnIndex = (_spawnIndex + 1) % _spawnPoints.Length;
+        return point;
     }
 }
